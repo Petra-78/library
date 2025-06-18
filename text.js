ab6e38b0911f3af106fdafb7ed7859a8126c0cde
Petra-78 <partos.petra.0305@gmail.com>
2025-06-18 15:09:40 +0200

    new book generation function in addBookToLibrary done

--- text.js
+++ text.js
@@ -1,12 +1,29 @@
+
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
     this.info = function() {
         return (`${this.title} by ${this.author}, ${this.pages} pages, ${read}`)
     }
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Not read yet");
-console.log(hobbit.info());
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+  let whatTitle = prompt("What is the title of the book?");
+  let whatAuthor = prompt("Who is the author?");
+  let howManyPages = prompt("How many pages is it?");
+  let haveRead = prompt("Have you read it yet?");
+
+  let newBook = new Book(whatTitle, whatAuthor, howManyPages, haveRead);
+  //the newBook objects current prototype is Object
+  myLibrary.push(newBook);
+
+  newBook = "";
+}
+
+
